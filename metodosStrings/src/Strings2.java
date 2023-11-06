81822710e4ed410242f1a15b6714954c1d68728c
DiEgOfercri <dfc25@icloud.com>
2023-11-06 13:16:11 +0100

    st2

---
+++ metodosStrings/src/Strings2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Strings2 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String frase;
+        String letra;
+        int contador = 0;
+
+
+        scanner.close();
+    }
+}
