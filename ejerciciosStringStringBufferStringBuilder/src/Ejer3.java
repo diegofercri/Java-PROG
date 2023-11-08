58030874be1ae9880936379a6f6cfc9fe3e113af
DiEgOfercri <dfc25@icloud.com>
2023-11-08 08:35:06 +0100

    Ejer3

--- ejerciciosStringStringBufferStringBuilder/src/Ejer3.java
+++ ejerciciosStringStringBufferStringBuilder/src/Ejer3.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Ejer3 {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce una palabra y la codificare en ASCII: ");
+        String input = scanner.nextLine();
+        String output = " ";
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            c++;
+            output += c;
+        }
+
+        System.out.println("El resultado es:" + output);
+
+        scanner.close();
     }
 }
