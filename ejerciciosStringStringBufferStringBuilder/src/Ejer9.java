938abcf3e0734661e1b49c366bc4183a991034a9
DiEgOfercri <dfc25@icloud.com>
2023-11-13 12:31:53 +0100

    coment

--- ejerciciosStringStringBufferStringBuilder/src/Ejer9.java
+++ ejerciciosStringStringBufferStringBuilder/src/Ejer9.java
@@ -5,15 +5,15 @@ public class Ejer9 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce una frase para buscar cauntas veces se repite una palabra dentro de ella: ");
-        String frase = scanner.nextLine().toLowerCase();
+        String frase = scanner.nextLine();
         System.out.print("Introduce la palabra que quieres buscar: ");
-        String palabra = scanner.nextLine().toLowerCase();
+        String palabra = scanner.nextLine();
         StringTokenizer tkn = new StringTokenizer(frase);
         int contador = 0;
 
         while (tkn.hasMoreTokens()) {
             String str = tkn.nextToken();
-            if (str.equals(palabra)) {
+            if (str.equalsIgnoreCase(palabra)) { // Con equalsIgnoreCase comprobamos que la palabra es igual ignorando mayusculas y minusculas.
                 contador++;
             }
         }
