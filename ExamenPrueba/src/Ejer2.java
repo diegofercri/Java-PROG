ddd3c7caa7b68864923b7fd1ac7717ff34d38df4
DiEgOfercri <dfc25@icloud.com>
2023-11-22 09:29:24 +0100

    comentario

--- ExamenPrueba/src/Ejer2.java
+++ ExamenPrueba/src/Ejer2.java
@@ -13,6 +13,7 @@ public class Ejer2 {
             /* 
             Los numeros en codigo unicode tienen un valor igual a su valor + 48, es decir para el numero 1 su unicode es 1+48=49.
             Por lo que si le restamos 48 a c tendremo el valor de cada numero sin realizar conversiones.
+            Aunque no es necesario ya que podemos comparar directamente su unidoce.
             */
             if (c - 48 >= anterior) {
                 anterior = c - 48;
