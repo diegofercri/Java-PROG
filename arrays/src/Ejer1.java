cadc71bacc28ee91df586f46b9fcd76d38629d3a
DiEgOfercri <dfc25@icloud.com>
2023-11-30 08:54:25 +0100

    Ejer2

--- arrays/src/Ejer1.java
+++ arrays/src/Ejer1.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Ejer1 {
+    /*
+     * En un array de 20 elementos hay almacenados números enteros. Haz un
+     * programa que pida un número y diga cuantas veces aparece el número dentro del
+     * array.
+     */
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce un número y te diré cuantas veces se repite: ");
