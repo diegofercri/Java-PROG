a9c0f288bbcc693cd4e85cf566a0ae585b8104cc
DiEgOfercri <dfc25@icloud.com>
2023-11-30 09:49:36 +0100

    Ejer2

---
+++ arrays/src/Ejer3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ejer3 {
+    /*
+     * Crea una agenda para almacenar nombres y direcciones de N personas. Después
+     * haz un programa que permita ir consultando el nombre y la dirección de las
+     * personas almacenadas.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("");
+    }
+}
