5e4c977ff641401e49d7459a614546c07b98073d
DiEgOfercri <dfc25@icloud.com>
2023-12-14 08:52:44 +0100

    Ejer3

--- arrays/src/Ejer3.java
+++ arrays/src/Ejer3.java
@@ -8,12 +8,25 @@ public class Ejer3 {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduce el nombre de la persona: ");
-        String nombre = scanner.nextLine();
-        System.out.print("Introduce la direccion de la persona: ");
-        String direccion = scanner.nextLine();
-        int[][] agenda;
+        System.out.print("Introduce el número de personas: ");
+        int n = scanner.nextInt();
 
-        agenda.
+        // Declaramos un array bidimensional y le indicamos cuantas filas y cuantas columnas debe tener
+        String[][] personas = new String[n][2];
+
+        // Para cada fila del array pedimos un nombre y una direccion
+        for (int i = 0; i < n; i++) {
+            System.out.print("Introduce el nombre de la persona " + (i+1) + ": ");
+            personas[i][0] = scanner.next();
+            System.out.print("Introduce la dirección de la persona " + (i+1) + ": ");
+            personas[i][1] = scanner.next();
+        }
+
+        // Pedimos el número de la fila de la persona a consultar
+        System.out.print("Introduce el índice de la persona a consultar: ");
+        int index = scanner.nextInt() - 1;
+        // Imprimimos en pantalla las columnas de a la fila indicada por el usuario
+        System.out.println("Nombre: " + personas[index][0]);
+        System.out.println("Dirección: " + personas[index][1]);
     }
 }
