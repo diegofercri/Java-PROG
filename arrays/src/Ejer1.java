a03d9a053e4597cff1f362efa728b7883542e26c
DiEgOfercri <dfc25@icloud.com>
2024-01-18 09:35:40 +0100

    del arrays

--- arrays/src/Ejer1.java
+++ arrays/src/Ejer1.java
@@ -7,20 +7,6 @@ public class Ejer1 {
      * array.
      */
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduce un número y te diré cuantas veces se repite: ");
-        int num = scanner.nextInt();
-        int[] numeros = { 5, 2, 3, 11, 5, 3, 11, 15, 15, 10, 11, 12, 5, 14, 15, 16, 15, 18, 5, 5 };
-        int contador = 0;
-
-        for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] == num) {
-                contador++;
-            }
-        }
-
-        System.out.println("El numero se repite " + contador + " veces.");
-
-        scanner.close();
+        
     }
 }
--- arrays/src/Ejer5.java
+++
@@ -1,5 +0,0 @@
-public class Ejer5 {
-    public static void main(String[] args) throws Exception {
-        
-    }
-}
