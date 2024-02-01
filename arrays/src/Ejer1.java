a920fd3eb4413ff37c343870a024ed415acc646d
DiEgOfercri <dfc25@icloud.com>
2024-02-01 09:03:37 +0100

    Ejer1

--- arrays/src/Ejer1.java
+++ arrays/src/Ejer1.java
@@ -9,17 +9,18 @@ public class Ejer1 {
     public static void main(String[] args) throws Exception {
         int num[] = {1,2,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         System.out.print("Introduce un numero y te dire cuantas veces se repite dentro de un array: ");
-        Scanner numAnterior = new Scanner (System.in);
+        Scanner input = new Scanner (System.in);
+        int numeroBuscado = input.nextInt();
         int contador = 0;
 
         for (int i = 0; i < num.length-1; i++) {
-            if (num[i] == numAnterior.nextInt()) {
+            if (num[i] == numeroBuscado) {
                 contador++;
             }
         }
 
-        System.out.println("El numero se repite: " + contador + " veces.");
+        System.out.println("El numero se repite " + contador + " veces.");
     
-        numAnterior.close();
+        input.close();
     }
 }
