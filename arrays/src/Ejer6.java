66ff8138e9cabe40d47b0acd35b46c54b2b992ee
DiEgOfercri <dfc25@icloud.com>
2024-02-03 12:24:29 +0100

    ejer6

--- arrays/src/Ejer6.java
+++ arrays/src/Ejer6.java
@@ -1,3 +1,21 @@
 public class Ejer6 {
-    /* */
+    /*
+     * Programa que rellene un array de 4x4 de la siguiente manera:
+     * 1 0 0 0
+     * 0 1 0 0
+     * 0 0 1 0
+     * 0 0 0 1
+    */
+    public static void main(String[] args) throws Exception {
+        int array[][] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
+        for (int i = 0; i < array.length; i++) {
+            array[i][i] = 1;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
