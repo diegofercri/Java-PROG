bb822c7cb745fb9b0b9c7fb3181eb4c4dfbbee9f
DiEgOfercri <dfc25@icloud.com>
2024-02-04 13:59:53 +0100

    Add mine detection logic to Ejer14.java

--- arrays2/src/Ejer14.java
+++ arrays2/src/Ejer14.java
@@ -37,6 +37,49 @@ public class Ejer14 {
             System.out.println(Arrays.toString(tablero[i]));
         }
 
+        // Pido coordenadas
+        System.out.print("Introduce las coordenadas de la fila: ");
+        int row = input.nextInt();
+        System.out.print("Introduce las coordenadas de la columna: ");
+        int col = input.nextInt();
+        
+        // Compruebo si hay mina
+        while (tablero[row][col] != 1) {
+            int minas = 0;
+            // Compruebo las minas alrededor
+            if (row > 0 && col > 0 && tablero[row - 1][col - 1] == 1) {
+                minas++;
+            }
+            if (row > 0 && tablero[row - 1][col] == 1) {
+                minas++;
+            }
+            if (row > 0 && col < 4 && tablero[row - 1][col + 1] == 1) {
+                minas++;
+            }
+            if (col > 0 && tablero[row][col - 1] == 1) {
+                minas++;
+            }
+            if (col < 4 && tablero[row][col + 1] == 1) {
+                minas++;
+            }
+            if (row < 4 && col > 0 && tablero[row + 1][col - 1] == 1) {
+                minas++;
+            }
+            if (row < 4 && tablero[row + 1][col] == 1) {
+                minas++;
+            }
+            if (row < 4 && col < 4 && tablero[row + 1][col + 1] == 1) {
+                minas++;
+            }
+            System.out.println("Hay " + minas + " minas alrededor");
+            System.out.print("Introduce las coordenadas de la fila: ");
+            row = input.nextInt();
+            System.out.print("Introduce las coordenadas de la columna: ");
+            col = input.nextInt();
+        }
+
+        System.out.println("!MINA¡");
+
         input.close();
     }
 }
