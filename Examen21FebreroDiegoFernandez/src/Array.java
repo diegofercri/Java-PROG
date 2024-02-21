76d1caea3bbb0df998f20912a746fd59efcf0951
Diego Fernández Criado <dfc25@icloud.com>
2024-02-21 08:45:25 +0100

    Examen 21 feb

---
+++ Examen21FebreroDiegoFernandez/src/Array.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class Array {
+    public static void main(String[] args) {
+        // Creo el array y lo relleno con números aleatorios
+        int[] array = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(50) + 1;
+        }
+
+        // Recorro el array tantas veces como elementos tenga y si en ese recorrido encuentro un numero igual sumo 1 al contador
+        int[] arrayContador = new int[51];
+        for (int i = 0; i < array.length; i++) {
+            int contador = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    contador++;
+                }
+            }
+            // Guardo las veces que se repite (contador) en la posición del array que es igual al número que estoy contando
+            arrayContador[array[i]] = contador;
+        }
+
+        // Recorro el array de contadores y me quedo con el que más se repite
+        int repetido = 0;
+        int maxArrayContador = 0;
+        for (int i = 1; i < arrayContador.length; i++) {
+            if (arrayContador[i] > maxArrayContador) {
+                repetido = i;
+                maxArrayContador = arrayContador[i];
+            }
+        }
+
+        System.out.println("El valor mas repetido es: " + repetido);
+    }
+}
