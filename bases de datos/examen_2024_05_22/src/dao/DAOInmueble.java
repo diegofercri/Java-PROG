80323fb683bef61740db683b008020c54f88fac5
Diego Fernández Criado <diegofercri25@gmail.com>
2024-05-22 09:23:23 +0200

    DAOs ended

--- bases de datos/examen_2024_05_22/src/dao/DAOInmueble.java
+++ bases de datos/examen_2024_05_22/src/dao/DAOInmueble.java
@@ -1,5 +1,112 @@
 package dao;
 
-public class DAOInmueble {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import conexion.DBConnection;
+import entidades.Inmueble;
+
+public class DAOInmueble {
+    public DAOInmueble() {
+		super();
+	}
+	
+	// add()
+    // get() que retorna todos los objetos Inmueble
+    // getListaInmueblesSinVender()  que  retorna  los  Listainmuebles  que  todavía  no  se  han vendido
+	
+	public void add(Inmueble i) {
+		// Obtenemos la conexion a la BBDD
+		Connection conexion = new DBConnection().getConexion();
+		
+		try {
+			// Usando un PreparedStatement
+			String sql = "INSERT INTO Inmueble VALUES(?,?,?,?,?,?)";
+			
+			// Creamos el Statement
+			PreparedStatement sentencia = conexion.prepareStatement(sql);
+			
+			// Sustituimos las interrogaciones
+			sentencia.setInt(1, i.getReferencia());
+			sentencia.setDate(2, i.getFechaAlta());
+            sentencia.setString(3, i.getTipo());
+            sentencia.setString(4, i.getProvincia());
+            sentencia.setInt(5, i.getSuperficie());
+            sentencia.setFloat(6, i.getPrecio());
+			
+			// Ejecutamos
+			sentencia.executeUpdate();
+			
+			// Cerramos la conexion
+			conexion.close();
+			
+			
+		} catch (SQLException e) {
+			System.out.print("Error insertando el Inmueble");
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Inmueble> get() {
+		// Obtenemos la conexion a la BBDD
+		Connection conexion = new DBConnection().getConexion();
+		ArrayList<Inmueble> ListaInmuebles = new ArrayList<Inmueble>();
+		
+		try {
+			// Usando un PreparedStatement
+			String sql = "SELECT referencia, fechaAlta, tipo, provincia, superficie, precio FROM Inmueble";
+			
+			// Creamos el Statement
+			PreparedStatement sentencia = conexion.prepareStatement(sql);
+			
+			ResultSet resultado = sentencia.executeQuery();
+			
+			while (resultado.next()) {
+				ListaInmuebles.add(new Inmueble(resultado.getInt(1), resultado.getDate(2), resultado.getString(3), resultado.getString(4), resultado.getInt(5), resultado.getFloat(6)));
+			}
+			
+			// Cerramos la conexion
+			conexion.close();
+			
+			
+		} catch (SQLException e) {
+			System.out.print("Error obteniendo Lista Inmuebles");
+			e.printStackTrace();
+		}
+		
+		return ListaInmuebles;
+	}
+
+	public ArrayList<Inmueble> getInmueblesSinVenver() {
+		// Obtenemos la conexion a la BBDD
+		Connection conexion = new DBConnection().getConexion();
+		ArrayList<Inmueble> ListaInmuebles = new ArrayList<Inmueble>();
+		
+		try {
+			// Usando un PreparedStatement
+			String sql = "SELECT referencia, fechaAlta, tipo, provincia, superficie, precio FROM Inmueble WHERE referencia NOT IN (SELECT referencia FROM Venta)";
+			
+			// Creamos el Statement
+			PreparedStatement sentencia = conexion.prepareStatement(sql);
+			
+			ResultSet resultado = sentencia.executeQuery();
+			
+			while (resultado.next()) {
+				ListaInmuebles.add(new Inmueble(resultado.getInt(1), resultado.getDate(2), resultado.getString(3), resultado.getString(4), resultado.getInt(5), resultado.getFloat(6)));
+			}
+			
+			// Cerramos la conexion
+			conexion.close();
+			
+			
+		} catch (SQLException e) {
+			System.out.print("Error obteniendo Lista Inmuebles");
+			e.printStackTrace();
+		}
+		
+		return ListaInmuebles;
+	}
 }
--- bases de datos/examen_2024_05_22/src/dao/DAOVenta.java
+++ bases de datos/examen_2024_05_22/src/dao/DAOVenta.java
@@ -1,5 +1,143 @@
 package dao;
 
-public class DAOVenta {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import conexion.DBConnection;
+import entidades.Venta;
+
+public class DAOVenta {
+	public DAOVenta() {
+		super();
+	}
+	
+	// El DAOVenta con los métodos: 
+	// add()
+	// get() que retorna todos los objetos Venta
+	// get() que retorna una venta pasando como parámetro una referencia
+	// get() que retorna todos los objetos Venta de un vendedor cuyo código se pasa como parámetro
+	
+	public void add(Venta v) {
+		// Obtenemos la conexion a la BBDD
+		Connection conexion = new DBConnection().getConexion();
+		
+		try {
+			// Usando un PreparedStatement
+			String sql = "INSERT INTO Venta VALUES(?,?,?)";
+			
+			// Creamos el Statement
+			PreparedStatement sentencia = conexion.prepareStatement(sql);
+			
+			// Sustituimos las interrogaciones
+			sentencia.setInt(1, v.getReferencia());
+			sentencia.setDate(2, v.getFechaVenta());
+			sentencia.setString(3, v.getVendedor());
+			
+			// Ejecutamos
+			sentencia.executeUpdate();
+			
+			// Cerramos la conexion
+			conexion.close();
+			
+			
+		} catch (SQLException e) {
+			System.out.print("Error insertando el Venta");
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Venta> get() {
+		// Obtenemos la conexion a la BBDD
+		Connection conexion = new DBConnection().getConexion();
+		ArrayList<Venta> ListaVentas = new ArrayList<Venta>();
+		
+		try {
+			// Usando un PreparedStatement
+			String sql = "SELECT referencia, fechaVenta, vendedor FROM Venta";
+			
+			// Creamos el Statement
+			PreparedStatement sentencia = conexion.prepareStatement(sql);
+			
+			ResultSet resultado = sentencia.executeQuery();
+			
+			while (resultado.next()) {
+				ListaVentas.add(new Venta(resultado.getInt(1), resultado.getDate(2), resultado.getString(3)));
+			}
+			
+			// Cerramos la conexion
+			conexion.close();
+			
+			
+		} catch (SQLException e) {
+			System.out.print("Error obteniendo Lista Ventas");
+			e.printStackTrace();
+		}
+		
+		return ListaVentas;
+	}
+	
+	public Venta get(int referencia) {
+		// Obtenemos la conexion a la BBDD
+		Connection conexion = new DBConnection().getConexion();
+		Venta v = null;
+		
+		try {
+			// Usando un PreparedStatement
+			String sql = "SELECT referencia, fechaVenta, vendedor FROM Venta WHERE codigo NOT IN (SELECT referencia FROM Venta)";
+			
+			// Creamos el Statement
+			PreparedStatement sentencia = conexion.prepareStatement(sql);
+			sentencia.setInt(1, referencia);
+			
+			ResultSet resultado = sentencia.executeQuery();
+			
+			if (resultado.next()) {
+				v = new Venta(resultado.getInt(1), resultado.getDate(2), resultado.getString(3));
+			}
+			
+			// Cerramos la conexion
+			conexion.close();
+			
+			
+		} catch (SQLException e) {
+			System.out.print("Error obteniendo el Venta");
+			e.printStackTrace();
+		}
+		
+		return v;
+	}
+	
+	public Venta get(String codigo) {
+		// Obtenemos la conexion a la BBDD
+		Connection conexion = new DBConnection().getConexion();
+		Venta v = null;
+		
+		try {
+			// Usando un PreparedStatement
+			String sql = "SELECT referencia, fechaVenta, vendedor FROM Venta, Vendedor WHERE Venta.vendedor = Vendedor.codigo AND Vendedor.codigo = ?";
+			
+			// Creamos el Statement
+			PreparedStatement sentencia = conexion.prepareStatement(sql);
+			sentencia.setString(1, codigo);
+			
+			ResultSet resultado = sentencia.executeQuery();
+			
+			if (resultado.next()) {
+				v = new Venta(resultado.getInt(1), resultado.getDate(2), resultado.getString(3));
+			}
+			
+			// Cerramos la conexion
+			conexion.close();
+			
+			
+		} catch (SQLException e) {
+			System.out.print("Error obteniendo el Venta");
+			e.printStackTrace();
+		}
+		
+		return v;
+	}
 }
